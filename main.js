575f285d7b8fd6a0a5d6285bbebbd54f30bb4d24
phobworks <phedwallace36th@live.com>
2025-01-19 14:04:29 +0100

    Filter completed

--- main.js
+++ main.js
@@ -28,6 +28,8 @@ async function petsArea() {
   petsData.forEach((pet) => {
     const clone = template.content.cloneNode(true);
 
+    clone.querySelector(".pet-card").dataset.species = pet.species;
+
     clone.querySelector("h3").textContent = pet.name;
     clone.querySelector(".pet-description").textContent = pet.description;
     clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
@@ -74,6 +76,14 @@ function handleButtonClick(event) {
   event.target.classList.add("active");
 
   // actually filter the pet down below
+  const currentFilter = event.target.dataset.filter;
+  document.querySelectorAll(".pet-card").forEach((el) => {
+    if (currentFilter == el.dataset.species || currentFilter == "all") {
+      el.style.display = "grid";
+    } else {
+      el.style.display = "none";
+    }
+  });
 }
 
 /* ====== Pets filter Section ends ======= */
