1c500b7f0d424be48aaedbb967189c92ee4cbcbf
phobworks <phedwallace36th@live.com>
2025-01-19 09:08:05 +0100

    Turned pet card into js template

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const template = document.querySelector("#pet-card-template");
+const wrapper = document.createDocumentFragment();
+
+/* ====== Weather Temperature Section Starts ======= */
+
 async function weatherStart() {
   const weatherPromise = await fetch(
     "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
@@ -11,3 +16,26 @@ async function weatherStart() {
   weatherTemp.textContent = ourTemperature;
 }
 weatherStart();
+
+/* ====== Weather Temperature Section Ends ======= */
+
+/* ====== Pets Select Section Starts ======= */
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    const clone = template.content.cloneNode(true);
+
+    clone.querySelector("h3").textContent = pet.name;
+
+    wrapper.appendChild(clone);
+  });
+
+  document.querySelector(".list-of-pets").appendChild(wrapper);
+}
+
+petsArea();
+
+/* ====== Pets Select Section Ends ======= */
