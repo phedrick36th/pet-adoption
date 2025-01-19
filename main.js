4782c3a23e78ed048b79a87115171e9d5f15abe4
phobworks <phedwallace36th@live.com>
2025-01-19 09:49:03 +0100

    Nearly finished card features.

--- main.js
+++ main.js
@@ -29,6 +29,12 @@ async function petsArea() {
     const clone = template.content.cloneNode(true);
 
     clone.querySelector("h3").textContent = pet.name;
+    clone.querySelector(".pet-description").textContent = pet.description;
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear);
+    clone.querySelector(".pet-card-photo img").src = pet.photo;
+    clone.querySelector(
+      ".pet-card-photo img"
+    ).alt = `A ${pet.species} named ${pet.name}.`;
 
     wrapper.appendChild(clone);
   });
@@ -37,5 +43,15 @@ async function petsArea() {
 }
 
 petsArea();
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+
+  if (age == 1) return "1 year old";
+  if (age == 0) return "Less than a year old";
+
+  return `${age} years old`;
+}
 
 /* ====== Pets Select Section Ends ======= */
